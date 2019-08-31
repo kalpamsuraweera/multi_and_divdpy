36029c9d440b75e97cb5b5871ac6961b546ec05a
kalpamsuraweera <48578445+kalpamsuraweera@users.noreply.github.com>
2019-08-31 10:20:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+num1,num2=eval(input("enter two numbers sperated in a comma(,) :-"))
+if num2!=0:
+    print("divded",num1/num2)
+else:
+    print("you can't use 0 to divid")
+print("multiple",num1*num2)
